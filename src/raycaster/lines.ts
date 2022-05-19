d4f6c88c0095cd3419749bc4bc3612cf097bc763
Sebastian Whalen <sebastian.whalen@mlt.ca>
2022-05-19 18:36:01 -0300

    Slight code cleanup and reorg

--- src/raycaster/lines.ts
+++ src/raycaster/lines.ts
@@ -48,6 +48,52 @@ function* getXIntercepts(x: number, y: number, direction: number): Generator<Pos
     }
 }
 
+const calculateXInterceptInformation = (direction: number, y: number, x: number) => {
+    const movingUp = (direction < 180);
+
+    const firstXInterceptY = (movingUp)
+        ? Math.trunc(y) + 1
+        : Math.ceil(y) - 1;
+
+    const xInterceptDy = (movingUp)
+        ? 1
+        : -1;
+
+    const xInterceptDx = Math.tan(direction * Math.PI / 180);
+
+    const firstXInterceptX = x + (firstXInterceptY - y) * xInterceptDx;
+
+    return {
+        firstXInterceptX,
+        firstXInterceptY,
+        xInterceptDy,
+        xInterceptDx
+    };
+}
+
+const calculateYInterceptInformation = (direction: number, x: number, xInterceptDy: number, y: number) => {
+    const movingRight = (direction < 90 || direction > 270);
+
+    const firstYInterceptX = (movingRight)
+        ? Math.trunc(x) + 1
+        : Math.ceil(x) - 1;
+
+    const yInterceptDx = (movingRight)
+        ? 1
+        : -1;
+
+    const yInterceptDy = 1 / xInterceptDy;
+
+    const firstYInterceptY = y + (firstYInterceptX - x) * yInterceptDy;
+
+    return {
+        firstYInterceptX,
+        firstYInterceptY,
+        yInterceptDx,
+        yInterceptDy
+    };
+}
+
 const getClosest = (startX: number, startY: number, p1: Position, p2: Position): Position => {
     const p1Distance = (p1.x - startX) ** 2 + (p1.y - startY) ** 2;
     const p2Distance = (p2.x - startX) ** 2 + (p2.y - startY) ** 2;
@@ -72,39 +118,19 @@ export function* getIntercepts(x: number, y: number, direction: number): Generat
     }
 
     //Calculate x intercept information
-    const movingUp = (direction < 180);
-
-    const firstXInterceptY = (movingUp)
-        ? Math.trunc(y) + 1
-        : Math.ceil(y) - 1
-
-    const xInterceptDy = (movingUp)
-        ? 1
-        : -1;
-
-    const xInterceptDx = Math.tan(direction * Math.PI / 180);
+    const { firstXInterceptX, firstXInterceptY, xInterceptDy, xInterceptDx } = calculateXInterceptInformation(direction, y, x);
 
     const nextX = {
-        x: x + (firstXInterceptY - y) * xInterceptDx,
+        x: firstXInterceptX,
         y: firstXInterceptY,
     };
 
     //Calculate y intercept information
-    const movingRight = (direction < 90 || direction > 270);
-
-    const firstYInterceptX = (movingRight)
-        ? Math.trunc(x) + 1
-        : Math.ceil(x) - 1;
-
-    const yInterceptDx = (movingRight)
-        ? 1
-        : -1
-
-    const yInterceptDy = 1 / xInterceptDy;
+    const { firstYInterceptX, firstYInterceptY, yInterceptDx, yInterceptDy } = calculateYInterceptInformation(direction, x, xInterceptDy, y);
 
     const nextY = {
         x: firstYInterceptX,
-        y: y + (firstYInterceptX - x) * yInterceptDy
+        y: firstYInterceptY
     };
 
     //Set position to the closest of the two.
