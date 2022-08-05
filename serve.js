d4cc3d6ac0270e8f2c59363a78efe406b886f738
Sebastian Whalen <sebwhalen@gmail.com>
2022-08-05 14:53:42 -0300

    Cleaned up serving code for better dev experience

--- serve.js
+++ serve.js
@@ -1,6 +1,6 @@
 const config = require('./esbuild.config.js');
 
-require('esbuild').serve({ servedir: 'dist' }, config)
+require('esbuild').serve({ servedir: 'dist', host: 'localhost', port: 8000 }, config)
     .then(server => {
-        console.log(`Server started at ${server.host}:${server.port}.`)
+        console.log(`Server started at http://${server.host}:${server.port}.`)
     });
