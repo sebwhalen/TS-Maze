5bc72adef69f533af97c730f73eee8882b6b3352
Sebastian Whalen <sebwhalen@gmail.com>
2022-08-05 14:55:20 -0300

    Fixed input bugs involving modifier keys

--- src/input/keyboardInput.ts
+++ src/input/keyboardInput.ts
@@ -2,11 +2,11 @@ export const inputHandler = (element: HTMLElement) => {
     const buffer: Set<string> = new Set();
 
     element.addEventListener('keydown', (e) => {
-        buffer.add(e.key);
+        buffer.add(e.key.toLowerCase());
     });
 
     element.addEventListener('keyup', (e) => {
-        buffer.delete(e.key);
+        buffer.delete(e.key.toLowerCase());
     });
 
     return {
